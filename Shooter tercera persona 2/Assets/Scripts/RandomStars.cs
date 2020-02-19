9e1b70742239c22eab20748733e0618da0727502
seloram <seloram@gmail.com>
2020-02-19 17:46:37 +0100

    menu con background y boton play

---
+++ Shooter tercera persona 2/Assets/Scripts/RandomStars.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStars : MonoBehaviour
+{
+    public GameObject stars;
+    private Vector3 randomVector;
+
+    public void Update()
+    {
+        randomVector = new Vector3(Random.Range(-30, 30), Random.Range(-15, -3), 35);
+
+        Instantiate(stars, randomVector, transform.rotation);
+    }
+}
---
+++ Shooter tercera persona 2/Assets/Scripts/StarsMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsMove : MonoBehaviour
+{
+    private float zMovement;
+    private float timeDestroy = 0.0f;
+
+    private void Start()
+    {
+        zMovement = Random.Range(-5, -20) * Time.deltaTime;
+    }
+    public void Update()
+    {
+        gameObject.transform.Translate(0, 0, zMovement);
+        timeDestroy += Time.deltaTime;
+        if (timeDestroy > 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
