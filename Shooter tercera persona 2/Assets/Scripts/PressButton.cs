e67d74bc03601f3c33e0a634aac4db7db946707c
seloram <seloram@gmail.com>
2020-02-20 17:38:00 +0100

    Menu Settings con animaciones

--- Shooter tercera persona 2/Assets/Scripts/PressButton.cs
+++ Shooter tercera persona 2/Assets/Scripts/PressButton.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PressButton : MonoBehaviour
 {
+    public GameObject Menu;
+    public GameObject SettingsPanel;
+    private Animator menuAnim;
+    private Animator settingsPanelAnim;
+    private void Awake()
+    {
+        menuAnim = Menu.GetComponent<Animator>();
+        settingsPanelAnim = SettingsPanel.GetComponent<Animator>();
+        //SettingsPanel.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +24,23 @@ public class PressButton : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Settings()
+    {
+        //SettingsPanel.SetActive(true);
+        menuAnim.SetBool("Close", true);
+        menuAnim.SetBool("Open", false);
+        settingsPanelAnim.SetBool("Open", true);
+        settingsPanelAnim.SetBool("Close", false);
+
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanelAnim.SetBool("Open", false);
+        settingsPanelAnim.SetBool("Close", true);
+        menuAnim.SetBool("Close", false);
+        menuAnim.SetBool("Open", true);        
     }
 
     public void PlayScene()
